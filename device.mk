1d351c5eaa7b5d60b4c8d41075562c79f9dc7056
Glen Kuhne <kuh@google.com>
2017-04-07 14:54:17 -0700

    psyche: overlay: Set default hotspot ssid
    Added a device specific overlay that changes the default name
    from "AndroidAp" to customized name
    
    Test: Builds, manually verified name change
    Bug: 36368950

--- device.mk
+++ device.mk
@@ -45,5 +45,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
+# WiFi
+PRODUCT_PACKAGES += \
+    TargetWifiOverlay
+
 # Inherit from vendor blobs
 $(call inherit-product, vendor/xiaomi/psyche/psyche-vendor.mk)
