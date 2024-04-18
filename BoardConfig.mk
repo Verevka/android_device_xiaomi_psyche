2aba5a5b41d03df4d0b18aa974f3ece1e80fd448
johnmart19 <johnivan19999@gmail.com>
2024-04-18 07:59:37 +0300

    psyche: Kernel: Switch to psyche_defconfig for kernel compilation

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,7 +20,7 @@ TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_psyche
 TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_psyche
 
 # Kernel
-TARGET_KERNEL_CONFIG += vendor/xiaomi/psyche.config
+TARGET_KERNEL_CONFIG := psyche_defconfig
 
 # OTA assert
 TARGET_OTA_ASSERT_DEVICE := psyche
