d56b2019a3b7736caf7669f63ccd08c06d99f579
Flicker372 <flicker372@outlook.com>
2024-07-12 19:01:25 +0800

    psyche: Import Display calibration blobs
    Change-Id: Ia052950e882b2c4f96a512464d641d9bec1dd8ec
    Signed-off-by: Flicker372 <flicker372@outlook.com>

--- device.mk
+++ device.mk
@@ -29,10 +29,6 @@ TARGET_SCREEN_WIDTH := 1080
 PRODUCT_PACKAGES += \
     libpiex_shim
 
-# Display
-PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,qdcm_calib_data_*.xml,$(LOCAL_PATH)/qdcm/,$(TARGET_COPY_OUT_VENDOR)/etc/)
-
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
