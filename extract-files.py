92c3a9f556713cfcf0e91927b433a740cb53bfa5
LuK1337 <priv.luk@gmail.com>
2024-12-10 23:07:45 +0100

    psyche: Enable elfchecks for camera libs
    Reference: I23190f0b83c9e28aa2624a7575544b0eaf5c83c8
    Change-Id: I18b0b07bf3a435dd449106653e3ca458b4f07b69

--- extract-files.py
+++ extract-files.py
@@ -21,6 +21,21 @@ blob_fixups: blob_fixups_user_type = {
         .regex_replace('.*seclabel u:r:batterysecret:s0\n', ''),
     'vendor/lib64/camera/components/com.mi.node.watermark.so': blob_fixup()
         .add_needed('libpiex_shim.so'),
+    (
+        'vendor/lib64/libMIAIHDRhvx_interface.so',
+        'vendor/lib64/libarcsoft_hdrplus_hvx_stub.so',
+        'vendor/lib64/libarcsoft_super_night_raw.so',
+        'vendor/lib64/libmialgo_rfs.so',
+    ): blob_fixup()
+        .clear_symbol_version('remote_handle_close')
+        .clear_symbol_version('remote_handle_invoke')
+        .clear_symbol_version('remote_handle_open')
+        .clear_symbol_version('remote_register_buf')
+        .clear_symbol_version('remote_register_buf_attr'),
+    'vendor/lib64/libarcsoft_super_night_raw.so' : blob_fixup()
+        .clear_symbol_version('rpcmem_alloc')
+        .clear_symbol_version('rpcmem_free')
+        .clear_symbol_version('rpcmem_to_fd'),
     'vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so': blob_fixup()
         .binary_regex_replace(b'\x9A\x0A\x00\x94', b'\x1F\x20\x03\xD5')
 }  # fmt: skip
