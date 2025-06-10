b86c0fd21e2fbd3d4cdb5900ae1a284d143dc8ab
Bruno Martins <bgcngm@gmail.com>
2025-06-10 18:44:42 +0200

    psyche: Set vendor init lib via soong config
    Change-Id: I8f7e6b987bee58dbf09ca9ad7cfd5a7e99e19c3b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,9 +15,6 @@ TARGET_SCREEN_DENSITY := 440
 # Device VINTF
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 
-# Init
-TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_psyche
-
 # Kernel
 TARGET_KERNEL_CONFIG := psyche_defconfig
 
