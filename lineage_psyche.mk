2f08843d90fc45482ef55fa24be70b1acc3baca0
Verevka <verevka86@gmail.com>
2025-10-06 13:52:16 +0300

    psyche: Spoof BuildFingerprint as Pixel Beta
    * It is necessary to pass integrity with the built-in Keybox Importer, as our stock fingerprint is not Valid

--- lineage_psyche.mk
+++ lineage_psyche.mk
@@ -24,5 +24,4 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="psyche-user 13 TKQ1.221114.001 V816.0.10.0.TLDCNXM release-keys" \
-    BuildFingerprint=Xiaomi/psyche/psyche:13/TKQ1.221114.001/V816.0.10.0.TLDCNXM:user/release-keys \
     BuildFlavor=psyche-user
